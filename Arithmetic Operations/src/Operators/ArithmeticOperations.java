5a135a7aaede7a34c2b2b32d27022cf7eaad9d2a
jegatheesh30 <jegatheesh30@selvaraj-Lenovo-E41-15>
2022-08-25 16:01:25 +0530

    jegatheesh (add scanner to get input)

--- Arithmetic Operations/src/Operators/ArithmeticOperations.java
+++ Arithmetic Operations/src/Operators/ArithmeticOperations.java
@@ -1,61 +1,85 @@
 package Operators;
+import java.util.Scanner;
 
 public class ArithmeticOperations {
 
 	public static void main(String[] args) {
 		{
-		        //Addition 
-				int a=23, b=9;
-				int sum = a+b;
-				
-				System.out.println("Addition");
-				System.out.println("a=21, b=32");
-				System.out.println("sum1: "+sum);
-				}
-				
-				//Addition for decimal
-				{
-				double a=32.23, b=12.2323;
-				int a1 = (int)a;
-				int b1 = (int)b;
-				int sum=a1+b1;
-				System.out.println("Addition decimal");
-				System.out.println("a=32.23, b=12.2323");
-				System.out.println("sum2: "+sum);
-				}
-				
-				{
-					//Subtraction
-				int a=23, b=45;
-				int sum = a-=b;
-				System.out.println("Substraction");
-				System.out.println("a=23, b=45");
-				System.out.println("sum3: "+sum);	
-				}
-				{
-				 //Multiplication
-				int a=21, b=32;
-				int sum = a*=b;
-				System.out.println("Mulitiplication");
-				System.out.println("a=21, b=32");
-				System.out.println("sum4: "+sum);
-				}
-				{
-				//Devision
-				int a=21, b=32;
-				int sum = a/=b;
-				System.out.println("Devision");
-				System.out.println("a=21, b=32");
-				System.out.println("sum5 :" + sum);
-				}
-				{
-				//remainder
-			    int a=21, b=32;
-		        int sum = a%=b;
-		        System.out.println("remainder");
-		        System.out.println("a=21, b=32");
-		        System.out.println("sum6: "+sum);
-				}
+	        //Addition 
+			System.out.println("Addition");
+			Scanner in=new Scanner(System.in);
+			int a, b;
+			System.out.println("Enter 1st integer");
+			a=in.nextInt();
+			System.out.println("Enter 2nd integer");
+			b=in.nextInt();
+			int sum = a+b;
+			System.out.println("a+b=?");
+			System.out.println("sum1: a+b ="+sum);
+			}
+			
+			//Addition for decimal
+			{
+			System.out.println("Addition decimal");
+			Scanner in=new Scanner(System.in);
+			System.out.println("Enter 1st double");
+			double a,b;
+			a=in.nextDouble();
+			System.out.println("Enter 2nd double");
+			b=in.nextDouble();
+			double sum= a+b;
+			System.out.println("sum2: a+b ="+sum);
+			}
+			
+			{
+				//Subtraction
+			System.out.println("Substraction");
+			Scanner in=new Scanner(System.in);
+			System.out.println("Enter 1st integer");
+			int a,b;
+			a=in.nextInt();
+			System.out.println("enter 2nd integer");
+			b=in.nextInt();
+			int sum = a-=b;
+			System.out.println("sum3: a-b = "+sum);	
+			}
+			{
+			 //Multiplication
+			System.out.println("Multiplication");
+			Scanner in=new Scanner(System.in);
+			System.out.println("Enter 1st integer");
+			int a,b;
+			a=in.nextInt();
+			System.out.println("Enter 2nd integer");
+			b=in.nextInt();
+			int sum = a*=b;
+			System.out.println("sum4: a*b = "+sum);
+			}
+			{
+			//Devision
+			System.out.println("Devision");
+			Scanner in =new Scanner(System.in);
+			System.out.println("Enter 1st integer");
+			int a,b;
+			a=in.nextInt();
+			System.out.println("Enter 2nd integer");
+			b=in.nextInt();
+			int sum = a/=b;
+			System.out.println("sum5: a/b =" + sum);
+			}
+			{
+			//remainder
+			System.out.println("Remainder");
+			Scanner ob=new Scanner(System.in);
+			System.out.println("Enter 1st integer");
+			int a,b;
+			a=ob.nextInt();
+			System.out.println("Enter 2nd integer");
+			b=ob.nextInt();
+	        int sum = a%=b;
+	        System.out.println("sum6: a%b  = "+sum);
+			}
+	       
 		       
 	}
 
