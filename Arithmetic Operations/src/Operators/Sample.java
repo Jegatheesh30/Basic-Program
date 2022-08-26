caf7e1bfb197abf8436ac7468d36d32837f1483a
jegatheesh30 <jegatheesh30@selvaraj-Lenovo-E41-15>
2022-08-26 18:38:00 +0530

    Swapping values

--- Arithmetic Operations/src/Operators/Sample.java
+++ Arithmetic Operations/src/Operators/Sample.java
@@ -3,8 +3,24 @@ package Operators;
 public class Sample {
 
 	public static void main(String[] args) {
+		
+
+						int n1=10,n2=23,n3;
+						System.out.println("before swap");
+						System.out.println("n1="+n1);
+						System.out.println("n2="+n2);
+						n3=n1;
+						n1=n2;
+						n2=n3;
+						System.out.println("after swaping");
+						System.out.println("n1  ="+  n1);
+						System.out.println("n2  =" + n2);
+					}
+
+				
+		
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
